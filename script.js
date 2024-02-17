f503b46f8503ba6577518e67246bf66147870add
AnujKaprawan <anujkp15@gmail.com>
2024-02-17 14:30:08 +0530

    JS file modified

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/** @format */
-
 let arr = [
   { id: 1, name: "john", age: "18", marks: 80 },
   { id: 2, name: "jack", age: "20", marks: 85 },
@@ -7,21 +5,36 @@ let arr = [
 ];
 
 function PrintStudentswithMap() {
-  //Write your code here , just console.log
+  const studentsOver50 = arr.filter(student => student.marks > 50);
+  console.log(studentsOver50);
 }
 
 function PrintStudentsbyForEach() {
-  //Write your code here , just console.log
+  arr.forEach(student => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
-  //Write your code here, just console.log
+  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newStudent);
+  console.log(newStudent);
 }
 
 function removeFailedStudent() {
-  //Write your code here, just console.log
+  const passedStudents = arr.filter(student => student.marks >= 50);
+  arr = passedStudents;
+  console.log(arr);
 }
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  const newStudents = [
+    { id: 4, name: "susan", age: "20", marks: 45 },
+    { id: 5, name: "peter", age: "21", marks: 60 },
+    { id: 6, name: "emma", age: "22", marks: 70 }
+  ];
+  arr = arr.concat(newStudents);
+  console.log(arr);
 }
